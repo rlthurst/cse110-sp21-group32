0bc2c59c5d8778efece36ded6a7b981aa27f23ed
rlthurst <rlthurst@ucsd.edu>
2021-06-28 18:52:47 -0700

    Purge

--- .eslintrc.js
+++
@@ -1,4 +0,0 @@
-// The example settings below tell ESLint use the eslint:recommended base configuration and the eslint-config-airbnb shared configuration.
-
-module.exports = {
-    extends: ["eslint:recommended"]};
